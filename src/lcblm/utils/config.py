b8d711de1a8874fd63c834e537854b1d8dcd2150
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2025-12-19 10:49:09 +0100

    feat(config): add structured config with pydantic models
    Introduce a new TOML-based configuration system for the project, using Pydantic models for type safety and validation. Add `config.toml` as the config file and implement a utility in `src/lcblm/utils/config.py` to parse and validate the configuration. Update dependencies to include `pydantic`. This change lays the foundation for consistent and robust configuration management across the codebase.

---
+++ src/lcblm/utils/config.py
@@ -0,0 +1,106 @@
+import sys
+import tomllib
+from enum import Enum, auto
+from pathlib import Path
+
+from pydantic import BaseModel
+
+from lcblm._logging import utils_logger as logger
+
+CONFIG_FILE = Path("config.toml")
+
+
+class EmbeddingConfig(BaseModel):
+    """Configuration for embedding extraction."""
+
+    batch_size: int
+    max_length: int = 256
+
+
+class FinetuneConfig(BaseModel):
+    """Configuration for finetuning of the original LLM head."""
+
+    batch_size: int
+
+
+class LinearTrainingConfig(BaseModel):
+    """Configuration for training a linear classifier."""
+
+    batch_size: int
+    p_dropout: float = 0
+    use_bias: bool = False
+
+
+class BaselineConfig(BaseModel):
+    """Configuration for the baselines."""
+
+    finetune: FinetuneConfig
+    train: LinearTrainingConfig
+
+
+class SAEConfig(BaseModel):
+    """Configuration for a Sparse AutoEncoder."""
+
+    batch_size: int
+    latent_dim_factor: int = 4
+
+
+class SAEHeadConfig(BaseModel):
+    """Configuration for a SAE + linear classifier."""
+
+    sae: SAEConfig
+    head: LinearTrainingConfig
+
+
+class LCBLMConfig(BaseModel):
+    pass
+
+
+class PerplexityConfig(BaseModel):
+    """Configuration for perplexity evaluation."""
+
+    llm: str
+
+
+class MetricsConfig(BaseModel):
+    """Configuration for metrics evaluation."""
+
+    perplexity: PerplexityConfig
+
+
+class Device(str, Enum):
+    """Available devices."""
+
+    cpu = auto()
+    cuda = auto()
+
+
+class Config(BaseModel):
+    """Configuration for the whole project."""
+
+    embedding: EmbeddingConfig
+    baseline: BaselineConfig
+    sae: SAEHeadConfig
+    lcblm: LCBLMConfig
+    metrics: MetricsConfig
+    backbone_llm: str = "mistralai/Mistral-7B-v0.1"
+    dataset: str = "sst2"
+
+
+def read_config(file_path: str | Path = CONFIG_FILE) -> Config:
+    """Read a configuration file."""
+    file_path = Path(file_path)
+
+    if not file_path.exists():
+        logger.error("Config file not found", extra={"file_path": file_path})
+        sys.exit(1)
+
+    with file_path.open("rb") as f:
+        config_dict = tomllib.load(f)
+
+    return Config.model_validate(config_dict)
+
+
+if __name__ == "__main__":
+    config = read_config()
+    print(config)
