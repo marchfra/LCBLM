0a87a64e64ffa445ee08c0d80a363de91fbcf95f
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2026-03-23 13:29:55 +0100

    feat: add TypedLinear Module

--- src/lcblm/typing.py
+++ src/lcblm/typing.py
@@ -1,7 +1,17 @@
-from typing import Protocol
+from typing import Protocol, TypeAlias
 
-from torch import Tensor
+from torch import Tensor, nn
 
 
 class TensorModule(Protocol):
     def forward(self, x: Tensor, /) -> Tensor: ...
+
+
+class TypedLinear(nn.Linear):
+    Output: TypeAlias = Tensor
+
+    def forward(self, input: Tensor) -> Output:  # noqa: A002
+        return super().forward(input)
+
+    def __call__(self, x: Tensor) -> Output:
+        return super().__call__(x)
