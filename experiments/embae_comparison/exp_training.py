a02770ef910f1d3b22cd1a6c1ee2641c784a4639
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2026-03-27 16:02:25 +0100

    chore: check model device

--- experiments/embae_comparison/exp_training.py
+++ experiments/embae_comparison/exp_training.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import copy
+import sys
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
@@ -118,6 +119,9 @@ def train_sparse_ae(
 
     """
     model = build_sparse_ae(n_concepts, cfg, ds_cfg)
+    print(model)
+    print(model.device)
+    sys.exit(0)
 
     geom_median = compute_tied_bias(X_train, 1)
     model.init_tied_bias(geom_median)
