d08c4d7b441c82246e964e3d00c46ede700b7f80
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2026-04-01 16:16:28 +0200

    feat: add get_device utility

--- src/lcblm/utils/__init__.py
+++ src/lcblm/utils/__init__.py
@@ -1,6 +1,11 @@
-from .pytorch import clamp_0_1, clamp_positive
+from .memory import free_gpu_memory
+from .pytorch import clamp_0_1, clamp_positive, get_device
+from .seed import set_seeds
 
 __all__ = [
     "clamp_0_1",
     "clamp_positive",
+    "free_gpu_memory",
+    "get_device",
+    "set_seeds",
 ]
